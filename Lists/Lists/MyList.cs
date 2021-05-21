89018f164bafd7c20f0430e40f28277419fdfb4d
mustafaguler3 <60480896+mustafaguler3@users.noreply.github.com>
2021-05-21 23:02:44 +0300

    week 4

---
+++ Lists/Lists/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public class MyList<T>
+    {
+        private T[] _items;
+
+        public MyList()
+        {
+            _items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] temp = _items;
+            _items = new T[_items.Length + 1];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                _items[i] = temp[i];
+            }
+        }
+    }
+}
