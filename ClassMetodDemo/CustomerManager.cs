4da77675d84c2f7d6f39314705b2abdcdb1ecf8b
mustafaguler3 <60480896+mustafaguler3@users.noreply.github.com>
2021-05-21 22:32:45 +0300

    week-3

---
+++ ClassMetodDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class CustomerManager
+    {
+        private static List<Customer> _customers;
+
+        public CustomerManager()
+        {
+            _customers = new List<Customer>();
+        }
+
+        public void Add(Customer customer)
+        {
+            _customers.Add(customer);
+            Console.WriteLine("Customer Added");
+        }
+
+        public List<Customer> List()
+        {
+            return _customers.ToList();
+        }
+
+        public void Delete(Customer customer)
+        {
+            _customers.Remove(customer);
+            Console.WriteLine("Customer deleted");
+        }
+    }
+}
---
+++ ClassMetodDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3;
+
+namespace ClassMetodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager manager = new CustomerManager();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "mustafa";
+            customer.Surname = "güler";
+
+            manager.Add(customer);
+
+            manager.List();
+
+            Console.ReadLine();
+        }
+
+    }
+}
